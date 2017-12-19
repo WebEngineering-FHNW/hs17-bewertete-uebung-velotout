5e0d6572de7aea6ec9437f492a7ccfcd41320f2f
Bastady John <you@example.com>
2017-12-19 13:34:03 +0100

    Origin attribute added

--- grails-app/domain/mvc/Media.groovy
+++ grails-app/domain/mvc/Media.groovy
@@ -4,15 +4,21 @@ abstract class Media{
 
     String title
     String autor
+    String origin
 
     static constraints = {
         title size: 3..30, blank: false
         autor size:3..30,blank:false
+        origin blank:false, inList: [FHNW_Windisch, FHNW_Olten, ETH_Zurich, UNI_Basel,HFU]
     }
 
     @Override
     String toString(){
-        return title+ " [" + autor+"]";
+        return title+ " [" + autor+"]"+"-"+origin;
     }
-
+    final static FHNW_Windisch  = "FHNW Windisch"
+    final static FHNW_Olten = "FHNW Olten"
+    final static ETH_Zurich = "ETH Zurich"
+    final static UNI_Basel = "Universität Basel"
+    final static HFU = "Hochschule Furtwangen"
 }
--- grails-app/init/mvc/BootStrap.groovy
+++ grails-app/init/mvc/BootStrap.groovy
@@ -16,15 +16,15 @@ class BootStrap {
         Person gabrielle = save(new Person(firstName: "Gabrielle", lastName: "Hure", student:true,email:"gabby@laposte.net",fakultaet:"FHNW Windisch"))
         Person stephane = save(new Person(firstName: "Stéphane", lastName: "Goldstein", student:true,email:"stephane.goldstein@hotmail.fr",fakultaet:"ETH Zurich"))
 
-        Media cd01 = save(new Cd(title:"Final Countdown",autor: "Europe",format:"mp3"))
-        Media cd02 = save(new Cd(title:"Highway to hell",autor: "ACDC",format:"mp3"))
-        Media cd03 = save(new Cd(title:"Nothing else matters",autor: "Metallica",format:"wav"))
-        Media cd04 = save(new Cd(title:"Je te promets",autor: "Johnny",format:"wav"))
+        Cd cd01 = save(new Cd(origin:"FHNW Windisch",title:"Final Countdown",autor: "Europe",format:"mp3"))
+        Cd cd02 = save(new Cd(origin:"FHNW Windisch",title:"Highway to hell",autor: "ACDC",format:"mp3"))
+        Cd cd03 = save(new Cd(origin:"FHNW Windisch",title:"Nothing else matters",autor: "Metallica",format:"wav"))
+        Cd cd04 = save(new Cd(origin:"FHNW Windisch",title:"Je te promets",autor: "Johnny",format:"wav"))
 
-        Book book01 = save(new Book(title:"Thérèse Raquin",autor: "Emile Zola",isbnID: "2-2122-6218-1", pages:"330",resume:"Die Geschichte von einem Mann",category:"ADULT",editor:"HATIER"))
-        Book book02 = save(new Book(title:"Astérix et Obélix",autor: "Gioscinni",isbnID: "3-2122-6218-2", pages:"240",resume:"Wie immer, werden Asterix und Obelix ein neue Abenteur leben in diesem Kapitel",category:"SCHOOL",editor:"HATIER"))
-        Book book03 = save(new Book(title:"Learn how to speak french",autor: "Europe",isbnID: "2-2122-6218-3", pages:"100",resume:"You find the french  people sexy ? We are agree with you. Try our new Method to speak French easily !",category:"SCHOOL",editor:"HATIER"))
-        Book book04 = save(new Book(title:"Final Book",autor: "Europe",isbnID: "3-2122-6218-4", pages:"140",resume:"Die Geschichte von einem Mann",category:"KIDS: 12-14",editor:"HATIER"))
+        Book book01 = save(new Book(origin:"FHNW Windisch",title:"Thérèse Raquin",autor: "Emile Zola",isbnID: "2-2122-6218-1", pages:"330",resume:"Die Geschichte von einem Mann",category:"ADULT",editor:"HATIER"))
+        Book book02 = save(new Book(origin:"FHNW Windisch",title:"Astérix et Obélix",autor: "Gioscinni",isbnID: "3-2122-6218-2", pages:"240",resume:"Wie immer, werden Asterix und Obelix ein neue Abenteur leben in diesem Kapitel",category:"SCHOOL",editor:"HATIER"))
+        Book book03 = save(new Book(origin:"FHNW Windisch",title:"Learn how to speak french",autor: "Europe",isbnID: "2-2122-6218-3", pages:"100",resume:"You find the french  people sexy ? We are agree with you. Try our new Method to speak French easily !",category:"SCHOOL",editor:"HATIER"))
+        Book book04 = save(new Book(origin:"FHNW Windisch",title:"Final Book",autor: "Europe",isbnID: "3-2122-6218-4", pages:"140",resume:"Die Geschichte von einem Mann",category:"KIDS: 12-14",editor:"HATIER"))
 
         Date today = new Date().clearTime();
 
